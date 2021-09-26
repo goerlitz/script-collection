166c4941883f524ff244b8c3b5b1b89e882552dc
Olaf Görlitz <olaf.goerlitz@gmail.com>
2021-09-27 01:28:28 +0200

    rename folder can now create multiple folders

--- shotwell/shotwell_rename_folder.sh
+++ shotwell/shotwell_rename_folder.sh
@@ -55,37 +55,9 @@ if [ "$verbose" -eq "1" ];then
     echo "path is $realdir"
 fi
 
-# get the event name for all images in the folder (if multiple events, use most frequent)
-event_name=$(sqlite3 ~/.shotwell_canon/data/photo.db "SELECT name FROM PhotoTable pt JOIN EventTable et ON pt.event_id = et.id WHERE filename LIKE '$realdir%' AND name is not NULL GROUP BY 1 ORDER BY count(*) desc LIMIT 1;")
+# get all event names
+event_names=$(sqlite3 $DB "SELECT DISTINCT name FROM PhotoTable pt JOIN EventTable et ON pt.event_id = et.id WHERE filename LIKE '$realdir%' AND name is not NULL;")
 
-# events start with the date - remove it, we only want the description
-event_name=${event_name#*20?? }
-
-# check that event name is not empty
-if [ -z "$event_name" ]; then
-    echo "event name is empty"
-    exit 1
-fi
-
-# strip old name from dir (if exists)
-lastpath=${realdir##*/}
-pathdate=${lastpath%% *}
-
-newdir="${realdir%/*}/$pathdate $event_name"
-
-if [ "$verbose" -eq "1" ];then
-    echo "new dir $newdir"
-fi
-
-if [ "$realdir" == "$newdir" ]; then
-    echo "STOPPING: new dir is the same - no need to rename!"
-    exit 1
-fi
-
-# first rename dir
-mv "$realdir" "$newdir"
-
-# -----------------
 
 # SQL result may contain SPACE - don't split
 IFS=$'\n'
@@ -93,34 +65,75 @@ IFS=$'\n'
 COUNT=0
 COUNT_BACKING=0
 
-# update database entry for every image in directory
-for img in $(sqlite3 $DB "SELECT filename FROM PhotoTable WHERE filename LIKE '$realdir%'";); do
-    file=${img##*/}
-    new_img="$newdir/$file"
+for e in $event_names; do
 
-    # first update filename in backing table if there is a developed image version
-    img_bk=$(sqlite3 $DB "SELECT filepath FROM BackingPhotoTable bpt JOIN PhotoTable pt ON bpt.id = pt.develop_camera_id WHERE filename = '$img'";)
-    if [ ! -z "$img_bk" ]; then
-        file_bk=${img_bk##*/}
-        new_img_bk="$newdir/$file_bk"
+    # events start with the date - remove it (assuming year is last part), we only want the description
+    event_name=${e#*20?? }
 
-        sqlite3 $DB "UPDATE BackingPhotoTable SET filepath='$new_img_bk' WHERE filepath = '$img_bk';"
-        (( COUNT_BACKING++ ))
-
-        if [ "$verbose" -eq "1" ];then
-            echo "B=> $new_img_bk"
-        fi
+    # check that event name is not empty
+    if [ -z "$event_name" ]; then
+        echo "SKIP: event name is empty"
+        continue
     fi
 
-    # then update filename in photo table
-    sqlite3 $DB "UPDATE PhotoTable SET filename='$new_img' WHERE filename = '$img';"
-    (( COUNT++ ))
+    # keep only date of currently processes directory
+    dirname=${realdir##*/}
+    dirdate=${dirname%% *}
+
+    newdir="${realdir%/*}/$dirdate $event_name"
 
     if [ "$verbose" -eq "1" ];then
-        echo "--> $new_img"
+        echo "NEW: $newdir"
     fi
-done
 
+    if [ "$newdir" == "$realdir" ]; then
+        echo "SKIP: new dir is the same - no need to rename!"
+        continue
+    fi
+
+    if [ -d "$newdir" ]; then
+        echo "SKIP: new dir already exists!"
+        continue
+    fi
+
+    mkdir "$newdir"
+
+    # get images for event and update database entry
+    for img in $(sqlite3 $DB "SELECT filename FROM PhotoTable pt JOIN EventTable et ON pt.event_id = et.id WHERE filename LIKE '$realdir%' AND name = '$e' ORDER BY filename;"); do
+        file=${img##*/}
+        new_img="$newdir/$file"
+
+        # move the file
+        mv "$img" "$new_img"
+
+        # first update filename in backing table if there is a developed image version
+        img_bk=$(sqlite3 $DB "SELECT filepath FROM BackingPhotoTable bpt JOIN PhotoTable pt ON bpt.id = pt.develop_camera_id WHERE filename = '$img'";)
+        if [ ! -z "$img_bk" ]; then
+            file_bk=${img_bk##*/}
+            new_img_bk="$newdir/$file_bk"
+
+            # finally move the file
+            mv "$img_bk" "$new_img_bk"
+
+            sqlite3 $DB "UPDATE BackingPhotoTable SET filepath='$new_img_bk' WHERE filepath = '$img_bk';"
+            (( COUNT_BACKING++ ))
+
+            if [ "$verbose" -eq "1" ];then
+                echo "B=> $new_img_bk"
+            fi
+        fi
+
+        # then update filename in photo table
+        sqlite3 $DB "UPDATE PhotoTable SET filename='$new_img' WHERE filename = '$img';"
+        (( COUNT++ ))
+
+        if [ "$verbose" -eq "1" ];then
+            echo "--> $new_img"
+        fi
+
+    done
+
+done
 unset IFS
 
 echo "DONE: $COUNT photos updated (+$COUNT_BACKING backing photos)"
