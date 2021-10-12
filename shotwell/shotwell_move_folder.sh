1760cd52fc18369eeb56b2608c6f748e0da96ec2
Olaf Görlitz <olaf.goerlitz@gmail.com>
2021-10-13 00:53:51 +0200

    improved moving of directories

--- shotwell/shotwell_move_folder.sh
+++ shotwell/shotwell_move_folder.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Move a specified image directory to a new locaion.
+# Move a specified image directory to a new location.
 
 # https://sookocheff.com/post/bash/parsing-bash-script-arguments-with-shopts/
 
 function usage {
         echo "Usage: $(basename $0) <source directory> <dest directory>" 2>&1
         echo '   -v   verbose'
+        echo '   -t   test (dry run)'
         echo '   -h   help (this text)'
         exit 1
 }
@@ -29,8 +30,8 @@ real_src_dir=$(realpath "$src_dir")
 dst_dir=$1; shift
 real_dst_dir=$(realpath "$dst_dir")
 
-# use source dir name as new dst subfolder
-real_dst_dir="$real_dst_dir/$src_dir"
+## use source dir name as new dst subfolder
+#real_dst_dir="$real_dst_dir/$src_dir"
 
 # check if directories exists
 [ ! -d "$real_src_dir/" ] && echo "Source dir does not exist! - $real_src_dir" && exit 1
@@ -39,14 +40,17 @@ real_dst_dir="$real_dst_dir/$src_dir"
 
 # set variables
 verbose=0
+dryrun=0
 
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
-while getopts "r:d:vh" opt; do
+while getopts "r:d:vth" opt; do
     case "$opt" in
     d)  DB=$OPTARG      # shotwell database
         ;;
     v)  verbose=1
+        ;;
+    t)  dryrun=1
         ;;
     h)  usage
         ;;
@@ -57,9 +61,13 @@ shift $((OPTIND-1))
 
 # ------------------
 
+if [ "$dryrun" -eq "1" ];then
+    echo "executing DRY RUN!"
+fi
+
 if [ "$verbose" -eq "1" ];then
-    echo "source path is $real_src_dir"
-    echo "dest path is $real_dst_dir"
+    echo "SRC>>> $real_src_dir"
+    echo "DST>>> $real_dst_dir"
 fi
 
 
@@ -71,28 +79,48 @@ IFS=$'\n'
 COUNT=0
 COUNT_BACKING=0
 
+COUNT_MISSING=0
+
 # update database entry for every image in directory
 for img in $(sqlite3 $DB "SELECT filename FROM PhotoTable WHERE filename LIKE '$real_src_dir%'";); do
-    file=${img##*/}
-    new_img="$real_dst_dir/$file"
+
+    new_img=${img/$real_src_dir/$real_dst_dir}
+
+    # check if new file exists
+    if [ ! -e "$new_img" ]; then
+        (( COUNT_MISSING++ ))
+        continue
+    fi
+
 
     # first update filename in backing table if there is a developed image version
     img_bk=$(sqlite3 $DB "SELECT filepath FROM BackingPhotoTable bpt JOIN PhotoTable pt ON bpt.id = pt.develop_camera_id WHERE filename = '$img'";)
     if [ ! -z "$img_bk" ]; then
-        file_bk=${img_bk##*/}
-        new_img_bk="$real_dst_dir/$file_bk"
 
-        sqlite3 $DB "UPDATE BackingPhotoTable SET filepath='$new_img_bk' WHERE filepath = '$img_bk';"
-        (( COUNT_BACKING++ ))
+        new_img_bk=${img_bk/$real_src_dir/$real_dst_dir}
+
+        # check if new file exists
+        if [ ! -e "$new_img_bk" ]; then
+            (( COUNT_MISSING++ ))
+            continue
+        fi
+
+        # update backing photo
+        if [ "$dryrun" -eq "0" ];then
+            sqlite3 $DB "UPDATE BackingPhotoTable SET filepath='$new_img_bk' WHERE filepath = '$img_bk';"
+            (( COUNT_BACKING++ ))
+        fi
 
         if [ "$verbose" -eq "1" ];then
             echo "B=> $new_img_bk"
         fi
     fi
 
-    # then update filename in photo table
-    sqlite3 $DB "UPDATE PhotoTable SET filename='$new_img' WHERE filename = '$img';"
-    (( COUNT++ ))
+    # update primary photo
+    if [ "$dryrun" -eq "0" ];then
+        sqlite3 $DB "UPDATE PhotoTable SET filename='$new_img' WHERE filename = '$img';"
+        (( COUNT++ ))
+    fi
 
     if [ "$verbose" -eq "1" ];then
         echo "--> $new_img"
@@ -100,6 +128,8 @@ for img in $(sqlite3 $DB "SELECT filename FROM PhotoTable WHERE filename LIKE '$
 done
 
 unset IFS
+
+echo "MISSING $COUNT_MISSING photos"
 
 echo "DONE: $COUNT photos updated (+$COUNT_BACKING backing photos)"
 
